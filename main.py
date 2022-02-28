daa09f98b22427fe52e02dc234cbbdf8f77c8635
akibalnika <77092253+anjuta777@users.noreply.github.com>
2022-02-28 12:17:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+i = [ 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+res = []
+for i in i:
+     res.append(pow(i, 3))
+
+
+print(res)
+
+i = [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1] 
+
+
+res = []
+for i in i:
+     res.append(pow(i, 2))
+
+
+print(res)
+
+import math
+i = [ 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8 , 7, 6, 5, 4, 3, 2, 1]
+
+
+res = []
+for i in i:
+     res.append(pow(i, 2))
+
+
+print(res)
